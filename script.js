a6dfa1aec8edb0e9a861e41ff0784687c24eb8bd
TeamFurious <teammfurious@gmail.com>
2026-02-24 01:25:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ toggle.addEventListener("click", () => {
     body.classList.toggle("light");
 
     if (body.classList.contains("light")) {
-        icon.classList.remove("fa-sun");
-        icon.classList.add("fa-moon");
-    } else {
         icon.classList.remove("fa-moon");
         icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
     }
 });
